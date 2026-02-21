bf8c4f823f20fabc98f9d752ec8da877b2a94932
Bassey John <jsbazman@gmail.com>
2026-02-21 12:29:00 +0100

    Revert "Added changes"
    This reverts commit 3cfdc8d8fd4f488a9809117a04de92bce81408e7.

--- src/analytics/constants.py
+++
@@ -1,8 +0,0 @@
-"""
-Constants used throughout the analytics application.
-"""
-from typing import Final
-
-# Batch size for bulk database operations
-# Larger batches improve performance but consume more memory
-BATCH_SIZE: Final[int] = 5000
